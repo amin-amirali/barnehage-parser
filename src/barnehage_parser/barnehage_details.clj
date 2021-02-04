3ee476133349eb21aba9ca88af6d07efe74ef340
Amin Amirali <amin.amirali@gmail.com>
2021-02-04 23:56:48 +0100

    Scrape more details

--- src/barnehage_parser/barnehage_details.clj
+++ src/barnehage_parser/barnehage_details.clj
@@ -1,6 +1,29 @@
 (ns barnehage-parser.barnehage-details
   (:require [net.cgrand.enlive-html :as html]))
 
+(defn- get-key-info
+  [content]
+  (let [all-key-info (->> (html/select content [:div.io-tile-common.io-tile-preschool.io-tile-preschool-information
+                                                :div.io-tile-content
+                                                :ul :li])
+                          (map html/text))
+        selected-key-info (mapv
+                            (vec all-key-info)
+                            [0 5 6 7 8 9])
+        keys [:barnehage_type :ages :kids_count :area_per_kid :kids_per_employee :pct_employees_with_degree]
+        keys-and-vals (zipmap keys selected-key-info)
+        clean-kids-count (update keys-and-vals :kids_count #(clojure.string/replace % #"Antall barn: " ""))
+        clean-area-kids (update clean-kids-count :area_per_kid #(re-find (re-matcher #"\d,\d+" %)))
+        clean-kids-employee (update clean-area-kids :kids_per_employee #(re-find (re-matcher #"\d,\d+" %)))
+        clean-pct-employees (update clean-kids-employee :pct_employees_with_degree #(re-find (re-matcher #"\d\d,\d+" %)))]
+    clean-pct-employees))
+
+(defn- get-address
+  [content]
+  (->> (html/select content [:div.contact-info :p :span])
+       (map html/text)
+       (clojure.string/join " ")))
+
 (defn- get-food-description
   [content]
   (-> (html/select content [:div.io-tile-preschool-prices])
@@ -66,9 +89,14 @@
      total-price (get-total-price content)
      food-price (get-food-price content)
      food-description (get-food-description content)
-     name (-> (html/select content [:h1.title]) first :content first)]
-    (assoc keys-and-values :total_price total-price
-                           :food_price food-price
-                           :food_description food-description
-                           :name name
-                           :url url)))
+     address (get-address content)
+     name (-> (html/select content [:h1.title]) first :content first)
+     key-info-map (get-key-info content)]
+    (-> keys-and-values
+        (assoc :total_price total-price
+               :food_price food-price
+               :food_description food-description
+               :name name
+               :url url
+               :address address)
+        (merge key-info-map))))
